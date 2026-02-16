a6b2cc2f3aac6f57961031c27e50590c1eaa6a09
serovaliz <serovaliz@gmail.com>
2026-02-16 20:26:31 +0300

    Объединила в единый файл

--- test_01.py
+++ test_01.py
@@ -1,39 +1,27 @@
-
 import time
-
-
-
-# webdriver это и есть набор команд для управления браузером
 from selenium import webdriver
-
-# импортируем класс By, который позволяет выбрать способ поиска элемента
 from selenium.webdriver.common.by import By
 
-# Инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Chrome()
 
-# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+def test_sum():
+    a = 5
+    b = 10
+    assert a != b
 
-# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://suninjuly.github.io/text_input_task.html")
-time.sleep(5)
+def test_01():
 
-# Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
-# Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать
-# Ищем поле для ввода текста
-textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
 
-# Напишем текст ответа в найденное поле
-textarea.send_keys("get()")
-time.sleep(6)
+    driver = webdriver.Chrome()
+    time.sleep(5)
 
-# Найдем кнопку, которая отправляет введенное решение
-submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
 
-# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
-submit_button.click()
-time.sleep(5)
+    driver.get("https://suninjuly.github.io/text_input_task.html")
+    time.sleep(5)
+    textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+    textarea.send_keys("get()")
+    time.sleep(6)
+    submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+    submit_button.click()
+    time.sleep(5)
+    driver.quit()
 
-# После выполнения всех действий мы должны не забыть закрыть окно браузера
-driver.quit()
